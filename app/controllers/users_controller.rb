64abb76732700b9eb7765f8cd6a663f15c81bb34
Joel Lewenstein <Joel@joel-lewensteins-macbook-pro-2.local>
2010-05-13 22:26:10 -0700

    Sass added, basic layout, controllers commented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,13 +73,13 @@ class UsersController < ApplicationController
 
   # DELETE /users/1
   # DELETE /users/1.xml
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
-    end
-  end
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  # 
+  #   respond_to do |format|
+  #     format.html { redirect_to(users_url) }
+  #     format.xml  { head :ok }
+  #   end
+  # end 
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,42 +1,42 @@
 class VotesController < ApplicationController
   # GET /votes
   # GET /votes.xml
-  def index
-    @votes = Vote.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @votes }
-    end
-  end
-
-  # GET /votes/1
-  # GET /votes/1.xml
-  def show
-    @vote = Vote.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @vote }
-    end
-  end
+  # def index
+  #     @votes = Vote.all
+  # 
+  #     respond_to do |format|
+  #       format.html # index.html.erb
+  #       format.xml  { render :xml => @votes }
+  #     end
+  #   end
+  # 
+  #   # GET /votes/1
+  #   # GET /votes/1.xml
+  #   def show
+  #     @vote = Vote.find(params[:id])
+  # 
+  #     respond_to do |format|
+  #       format.html # show.html.erb
+  #       format.xml  { render :xml => @vote }
+  #     end
+  #   end  
 
   # GET /votes/new
   # GET /votes/new.xml
-  def new
-    @vote = Vote.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @vote }
-    end
-  end
+  # def new
+  #     @vote = Vote.new
+  # 
+  #     respond_to do |format|
+  #       format.html # new.html.erb
+  #       format.xml  { render :xml => @vote }
+  #     end
+  #   end  
 
   # GET /votes/1/edit
-  def edit
-    @vote = Vote.find(params[:id])
-  end
-
+  # def edit
+  #   @vote = Vote.find(params[:id])
+  # end
+  #     
   # POST /votes
   # POST /votes.xml
   def create
@@ -89,13 +89,13 @@ class VotesController < ApplicationController
 
   # DELETE /votes/1
   # DELETE /votes/1.xml
-  def destroy
-    @vote = Vote.find(params[:id])
-    @vote.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(votes_url) }
-      format.xml  { head :ok }
-    end
-  end
+  # def destroy
+  #   @vote = Vote.find(params[:id])
+  #   @vote.destroy
+  # 
+  #   respond_to do |format|
+  #     format.html { redirect_to(votes_url) }
+  #     format.xml  { head :ok }
+  #   end
+  # end   
 end
