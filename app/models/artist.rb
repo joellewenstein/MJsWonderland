11d970759d76972bb4e9ed7b9a253a4266ef4d64
Joel Lewenstein <Joel@joel-lewensteins-macbook-pro-2.local>
2010-05-19 23:08:48 -0700

    Styling

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,8 @@ class Artist < ActiveRecord::Base
   validates_uniqueness_of :jambase_id
   
   has_and_belongs_to_many :events
+  
+  def album_image
+    "http://fashionducolor.files.wordpress.com/2009/11/lady-gaga-album_cover-the_fame.jpg"
+  end
 end
